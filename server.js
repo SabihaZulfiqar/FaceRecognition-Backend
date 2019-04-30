22a86d2857952756fcb8c799396cf60a61577313
SabihaZulfiqar <sabiha.zulfiqar@gmail.com>
2019-04-30 14:37:26 +0400

    Updated Project

--- server.js
+++ server.js
@@ -11,10 +11,13 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'abcdefg',
+    database : 'facerecognition'
   }
 });
+
 
 const app = express();
 
