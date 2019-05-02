77b94cbd29e5e203bc06a8ef9ab17488633207c8
Sabiha Zulfiqar <36043559+SabihaZulfiqar@users.noreply.github.com>
2019-05-02 21:59:06 +0400

    Update server.js

--- server.js
+++ server.js
@@ -23,11 +23,11 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', ( req , res ) => {	res.send('This is working!'); })
-app.get('/signin' , (req, res) => {signin.handleSignin(req, res, db, bcrypt)} )
-app.get('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)} )
+app.post('/signin' , (req, res) => {signin.handleSignin(req, res, db, bcrypt)} )
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)} )
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)} )
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
-app.get('/imageurl' , (req, res) => {image.handleApiCall(req, res)} )
+app.post('/imageurl' , (req, res) => {image.handleApiCall(req, res)} )
 
 
 app.listen(process.env.PORT || 3000, () => {
